5c52c47f0dd726daa5febda10024c033def1fa09
Saifullah <saifullahkhaliq@gmail.com>
2019-08-28 23:24:41 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,14 +20,6 @@ $(document).ready(function () {
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
   }
-  
-  if (window.location.hash && window.location.pathname == '{{site.baseurl}}/2016/kiki/') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-    
-  if (window.location.hash && window.location.hash == '#about') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
     $('.panel-cover').addClass('panel-cover--collapsed')
